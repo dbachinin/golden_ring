7ab370a0036cf5cecb6ca6c29a707eda5bb56c06
dbachinin <dbachinin@ya.ru>
2017-12-03 22:02:10 +0300

    fix seeds after devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+devise_scope :user do
+get "/login" => "devise/sessions#new"
+get "/logout" => "devise/sessions#destroy"
+get "/signup" => "devise/registrations#new"
+end
+
+
   resources :locations
   resources :rooms
   resources :fotos
