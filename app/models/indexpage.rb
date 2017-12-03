2bda24d69eb7083c208497cd0f15ffe690cb92a4
dbachinin <dbachinin@ya.ru>
2017-12-03 21:06:48 +0300

    fix seeds

--- app/models/indexpage.rb
+++ app/models/indexpage.rb
@@ -1,9 +1,8 @@
 class Indexpage < ApplicationRecord
-	EMAIL = 'dbachinin@ya.ru'
-	validates :email_is_true
+# 	EMAIL = 'dbachinin@ya.ru'
+# 	validate :email_is_true
 
-private
-def email_is_true
-		return true if email.include?(EMAIL)
-	end
+# def email_is_true
+# 		return true if email.include?(EMAIL)
+# 	end
 end
